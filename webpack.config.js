65e5d0d5c3cfd23fc547053e25232b5b055fecc5
mlynam <mlynam@iteamnm.com>
2017-06-21 20:28:35 -0600

    chore: linting webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,6 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: `${pkg.name}.[hash:8].js`
-  }
+    filename: `${pkg.name}.[hash:8].js`,
+  },
 };
